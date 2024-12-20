00e12457ff3544797f9570987fb0f076bee5c788
Celso Nery <celso.nery@gmail.com>
2024-12-20 13:33:41 -0300

    fix: Trying fix github pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   head: [['link', {rel: 'icon', href: '/images/hero.png'}]],
   bundler: viteBundler(),
-  base: 'https://celsonery.github.io/english/',
+  base: '/',
   theme: defaultTheme({
     docsRepo: 'https://github.com/celsonery/english',
     docsBranch: 'main',
